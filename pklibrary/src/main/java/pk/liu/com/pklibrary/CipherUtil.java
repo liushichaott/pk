d696de9d16c2db962e07d8b62c8f9bce49b8c3c5
liushichaott <15639257496@163.com>
2018-03-02 14:31:50 +0800

    添加随机数方法

--- pklibrary/src/main/java/pk/liu/com/pklibrary/CipherUtil.java
+++ pklibrary/src/main/java/pk/liu/com/pklibrary/CipherUtil.java
@@ -47,4 +47,29 @@ public class CipherUtil {
             return null;
         }
     }
+
+    public static String createRandom(boolean numberFlag, int length) {
+        String retStr = "";
+        String strTable = numberFlag ? "0123456789" : "0123456789abcdefghijklmnopqrstuvwxyz";
+        int len = strTable.length();
+        boolean bDone = true;
+        do {
+            retStr = "";
+            int count = 0;
+            for (int i = 0; i < length; i++) {
+                double dblR = Math.random() * len;
+                int intR = (int) Math.floor(dblR);
+                char c = strTable.charAt(intR);
+                if (('0' <= c) && (c <= '9')) {
+                    count++;
+                }
+                retStr += strTable.charAt(intR);
+            }
+            if (count >= 2) {
+                bDone = false;
+            }
+        } while (bDone);
+
+        return retStr;
+    }
 }
